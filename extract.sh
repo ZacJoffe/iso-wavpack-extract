5cd18181dc9666280c50c54d28ec0c3a4b6c681b
ZacJoffe <zacharyjoffe@gmail.com>
2020-04-05 19:16:57 -0400

    Tabs -> spaces pivot

--- extract.sh
+++ extract.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 for i in *; do
-	if [[ -f "$i" && "$i" != "extract.sh" ]]; then
-		dir="${i::-7}"
-		mkdir "$dir"
-		cp "$i" "$dir/."
-		cd "$dir"
-		7z x "$i"
-		rm "$i"
-		cd ..
-	fi
+    if [[ -f "$i" && "$i" != "extract.sh" ]]; then
+        dir="${i::-7}"
+        mkdir "$dir"
+        cp "$i" "$dir/."
+        cd "$dir"
+        7z x "$i"
+        rm "$i"
+        cd ..
+    fi
 done
