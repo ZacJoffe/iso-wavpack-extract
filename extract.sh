8e86bdbbe040b62eba41f6dcbede139717aaaa27
ZacJoffe <zacharyjoffe@gmail.com>
2020-04-06 22:51:36 -0400

    Better file matching

--- extract.sh
+++ extract.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 for i in *; do
-    if [[ -f "$i" && "$i" != "extract.sh" ]]; then
+    if [[ -f "$i" && "$i" == *.iso.wv ]]; then
         dir="${i::-7}"
         mkdir "$dir"
         cp "$i" "$dir/."
